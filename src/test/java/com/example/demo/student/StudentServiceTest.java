5f7207cba247b9c23c104bb3c9acbb45bb7a8415
Pedro Carara <cararax@gmail.com>
2022-02-05 14:50:31 -0300

    Adicionado unit test de StudentService com repository mokado e argumentcaptor

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,8 +1,9 @@
 package com.example.demo.student;
 
 
+import com.example.demo.student.exception.BadRequestException;
+import com.example.demo.student.exception.StudentNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -10,7 +11,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,7 +59,49 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void deleteStudent() {
+    void willThownWhenEmailIsTaken() {
+        //given
+        Student student = new Student(
+                "jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE);
+        given(studentRepository.selectExistsEmail(anyString()))
+                .willReturn(true);
+        //when
+        //then
+        assertThatThrownBy(() -> underTest.addStudent(student))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+        verify(studentRepository, never()).save(any());
+
+    }
+
+
+    @Test
+    void canDeleteStudent() {
+        // given
+        long id = 10;
+        given(studentRepository.existsById(id))
+                .willReturn(true);
+        // when
+        underTest.deleteStudent(id);
+
+        // then
+        verify(studentRepository).deleteById(id);
+    }
+
+    @Test
+    void willThrowWhenDeleteStudentNotFound() {
+        // given
+        long id = 10;
+        given(studentRepository.existsById(id))
+                .willReturn(false);
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.deleteStudent(id))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Student with id " + id + " does not exists");
+
+        verify(studentRepository, never()).deleteById(any());
     }
 }
