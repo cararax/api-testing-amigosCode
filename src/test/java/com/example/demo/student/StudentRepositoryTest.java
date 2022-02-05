02cfcc79ab6903b46457678f0650edb2c15e8af5
Pedro Carara <cararax@gmail.com>
2022-02-05 11:54:03 -0300

    Adicionado unit test de StudentRepository, adicionado h2 db

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,18 +1,25 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void TearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
-    void itShoudCheckIfStudentExistsByEmail() {
+    void itShoudCheckWhenStudentEmailExists() {
         //given
         String email = "jamila@gmail.com";
         Student student = new Student(
@@ -27,6 +34,17 @@ class StudentRepositoryTest {
 
         //then
         assertThat(expected).isTrue();
+    }
 
+    @Test
+    void itShoudCheckWhenStudentEmailDoesNotExists() {
+        //given
+        String email = "jamila@gmail.com";
+
+        //when
+        Boolean expected = underTest.selectExistsEmail(email);
+
+        //then
+        assertThat(expected).isFalse();
     }
 }
